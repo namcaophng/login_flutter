0936fa1c570238c424442cae2e0a9b4fbf9d7eb5
namcaophng <namcaophng@gmail.com>
2021-07-01 17:25:14 +0700

    Use getX to navigation

--- lib/view/screen/home/view/home_view.dart
+++ lib/view/screen/home/view/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:login/constant/page_constant.dart';
+import 'package:login/controller/page_navigation_controller/page_navigation_controller.dart';
 
 class HomeViewPage extends StatefulWidget {
   const HomeViewPage({Key? key}) : super(key: key);
@@ -11,9 +14,22 @@ class _HomeViewPageState extends State<HomeViewPage> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'Home Page',
-      ),
+      child: Column(
+        children: [
+          Text(
+            'Home Page',
+          ),
+          TextButton(
+            onPressed: _navigateBack,
+            child: Text("Back"),
+          )
+        ],
+      )
     );
+  }
+
+  void _navigateBack() {
+    final navigator = Get.find<NavigationPageController>();
+    navigator.changeActivePage(PageConstant.LOGIN_PAGE);
   }
 }
